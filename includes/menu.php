f53df9f482deb759e80fb3e8762485609b856e68
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2024-06-25 14:56:13 +0200

    #Improvment: Melhoria no menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,20 +1,23 @@
 <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #1d1b1b;">
   
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" style="color: white;">Sistema de Cadastro</a>
+    <a class="navbar-brand" href="#" style="color: white;">
+    <img src="../images/checklist_9658801.png" alt="" height="37px" width="37px"> 
+    Sistema de Cadastro</a>
+        
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown" style="margin-left: 60%;">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="../index.php" style="color: white;">Home</a>
+          <a class="nav-link" aria-current="page" href="../index.php" style="color: #00BBF0;">HOME</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../Tarefas/FRMListarTarefas.php" style="color: white;">Tarefas</a>
+          <a class="nav-link" href="../Tarefas/FRMListarTarefas.php" style="color: white;">TAREFAS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../Clientes/FRMListarClientes.php" style="color: white;">Clientes</a>
+          <a class="nav-link" href="../Clientes/FRMListarClientes.php" style="color: white;">CLIENTES</a>
         </li>
        
       </ul>
